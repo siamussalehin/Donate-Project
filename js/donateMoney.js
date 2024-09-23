21fc7baff7badbeb07504115e9feaf12d289855a
Siamus Salehin <siamussalehin20000@gmail.com>
2024-09-23 13:02:54 -0700

    completing 2

--- js/donateMoney.js
+++ js/donateMoney.js
@@ -2,18 +2,45 @@ document.getElementById('btn-donate-money').addEventListener('click', function()
     
     
     const donateMoney = getInputById('input-donate');
+    const gettingAmount = calledDonateBtn('accountBalance', 'input-donate')
+    
+
+    if(isNaN(donateMoney)){
+        alert('Failed to donate');
+        return;
+    }
     
 
     if(donateMoney > 0){
         const balance = getTextById('balance');
+        
+        
+        
         const newBalance = balance + donateMoney;
         document.getElementById('balance').innerText = newBalance;
         const accountBalance = document.getElementById('account-balance').innerText;
         const newAccountBalance = accountBalance - donateMoney;
         document.getElementById('account-balance').innerText = newAccountBalance;
+
+        //add to donate history//
+        const div = document.createElement('div');
+        div.classList.add('w-10/12 mx-auto border-solid border-2 rounded-lg p-5');
+        div.innerHTML = `
+        <h4 class ="text-2xl font-bold"> ${donateMoney} Tk is Donated for Donate for Flood at Noakhali, Bangladesh</h4>
+        <p class = "gray-color">Date: ${new Date()}.toLocalString()}
+        (Bangladesh Standard Time);
+        `
+        document.getElementById('transaction-container').appendChild(div);
+        // const p = document.createElement('p');
+        // p.innerText = `${donateMoney}`
+        // document.getElementById('transaction-container').appendChild(p);
         
-       
     }
+    else if(donateMoney > accountBalance){
+        alert('You do not have enough money to donate');
+        return;
+    }
+    
     else{
         alert('Failed to donate')
     }
--- js/feature.js
+++ js/feature.js
@@ -1,9 +1,7 @@
 document.getElementById('show-donate-section').addEventListener('click', function(){
-    console.log('show donate form cliked');
     showSectionById('donate-section');
 })
 
 document.getElementById('show-history-section').addEventListener('click', function(){
-    console.log('cliked');
     showSectionById('history-section')
 })
